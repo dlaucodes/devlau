0cb22e3aa88a2db68e2895b181b5afc9cf2e4cad
Chun Lau <dlaucodes@gmail.com>
2024-02-06 16:45:34 -0500

    create hero info

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,9 +10,9 @@ import Social from './Social';
 
 const Hero = () => {
   return (
-    <section>
+    <section className='py-12 xl:py-24 h-[84vh] xl:pt-28 bg-hero bg-no-repeat bg-bottom bg-cover dark:bg-none'>
         <div className="container mx-auto">
-            <div>
+            <div className="flex justify-between gap-x-8">
                 <div>
                     text
                 </div>
@@ -20,7 +20,7 @@ const Hero = () => {
                     image
                 </div>
             </div>
-            <div className='hidden md:flex'>
+            <div className='hidden md:flex absolute left-2/4 bottom-44 xl:bottom-12 animate-bounce'>
                 <RiArrowDownSLine className="text-3xl text-primary" />
             </div>
 
--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -1,9 +1,24 @@
-'use client'
+import Link from "next/link";
 
 const MobileNav = () => {
   return (
-    <div>MobileNav</div>
-  )
-}
+    <div>
+      <svg
+        className="w-6 h-6"
+        fill="none"
+        stroke="white"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="1.5"
+          d="M4 6h16M4 12h16M4 18h16"
+        ></path>
+      </svg>
+    </div>
+  );
+};
 
-export default MobileNav
+export default MobileNav;
