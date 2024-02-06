bc13bfc05638eeb605d38d68677fe831d3287a1b
Chun Lau <dlaucodes@gmail.com>
2024-02-05 22:13:13 -0500

    style header elements

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,16 +4,36 @@ import ThemeToggler from "./ThemeToggler";
 import Logo from "./Logo";
 import Nav from "./Nav";
 import MobileNav from "./MobileNav";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const [header, setHeader] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    const scrollYPos = window.addEventListener("scroll", () => {
+      window.scrollY > 50 ? setHeader(true) : setHeader(false);
+    });
+    return () => window.removeEventListener("scroll", scrollYPos);
+  });
   return (
-    <header>
+    <header
+      className={`${
+        header
+          ? "py-4 bg-white shadow-lg dark:bg-accent"
+          : "py-6 dark:bg-transparent"
+      } sticky top-0 z-30 transition-all`}
+    >
       <div className="container mx-auto">
         <div className="flex justify-between items-center">
           <Logo />
 
           <div className="flex items-center gap-x-6">
-            <Nav containerStyles="hidden xl:flex gap-x-8 items-center" linkStyles='relative hover:text-primary transition-all' underlineStyles='absolute left-0 top-full h-[2px] bg-primary w-full' />
+            <Nav
+              containerStyles="hidden xl:flex gap-x-8 items-center"
+              linkStyles="relative hover:text-primary transition-all"
+              underlineStyles="absolute left-0 top-full h-[2px] bg-primary w-full"
+            />
             <ThemeToggler />
             <div className="flex xl:hidden">
               <MobileNav />
