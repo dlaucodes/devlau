483046ab4a817796b04d093cd93d35269c107a08
Chun Lau <dlaucodes@gmail.com>
2024-02-20 22:48:50 -0500

    update theme toggle

--- components/ThemeToggler.tsx
+++ components/ThemeToggler.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { Button } from "./ui/button";
-import { MoonIcon, SunIcon } from "@radix-ui/react-icons";
+import {FaSun, FaMoon } from "react-icons/fa";
 import { useTheme } from "next-themes";
 
 const ThemeToggler = () => {
@@ -12,8 +12,8 @@ const ThemeToggler = () => {
       size='icon' 
       onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
       >
-        <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-        <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
+        <FaSun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
+        <FaMoon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 text-white" />
       </Button>
     </div>
   );
