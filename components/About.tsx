d2dd436b105e1a14e4a2a8f56f7cf242100d88a3
Chun Lau <dlaucodes@gmail.com>
2024-03-06 23:54:04 -0500

    add icons

--- components/About.tsx
+++ components/About.tsx
@@ -1,4 +1,34 @@
+import Image from 'next/image';
+import {Tabs, TabsContent, TabsList, TabsTrigger} from '@/components/tabs'
 
+import {User2, HomeIcon, MailIcon, PhoneCall, GraduationCap, Calendar, Briefcase} from 'lucide/react';
+
+const infoData = [
+    {
+        icon: <User2 size={30} />,
+        text: 'David Lau',
+    },
+      {
+        icon: <PhoneCall size={30} />,
+        text: 'David Lau',
+    },
+      {
+        icon: <User2 size={30} />,
+        text: 'David Lau',
+    },
+      {
+        icon: <User2 size={30} />,
+        text: 'David Lau',
+    },
+      {
+        icon: <User2 size={30} />,
+        text: 'David Lau',
+    },
+      {
+        icon: <User2 size={30} />,
+        text: 'David Lau',
+    },
+]
 
 const About = () => {
   return (
