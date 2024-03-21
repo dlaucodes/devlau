f874ece0a4530148283d748f85c71d0a22bb7394
Chun Lau <dlaucodes@gmail.com>
2024-03-20 23:53:22 -0400

    change font

--- components/MobileNav.tsx
+++ components/MobileNav.tsx
@@ -16,7 +16,7 @@ const MobileNav = () => {
       </SheetTrigger>
       <SheetContent>
         <div className="flex flex-col items-center justify-between h-full py-8">
-          <div className="flex flex-col items-center gap-y-32">
+          <div className="flex flex-col items-center gap-y-12">
             <Logo />
     
             <Nav
