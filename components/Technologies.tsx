bfc61293cb290b2ddbc72aa8ce0c661444491d4f
Chun Lau <dlaucodes@gmail.com>
2024-03-28 00:00:56 -0400

    complete about me section

--- components/Technologies.tsx
+++ components/Technologies.tsx
@@ -1,114 +1,191 @@
-import React from 'react';
-import Image from 'next/image';
+import React from "react";
+import Image from "next/image";
 
 const Technologies = () => {
   return (
-    <div className="grid grid-cols-4 w-full justify-items-center gap-5 mt-10">
-        <div>
-        <Image src='/assets/icons/nextjs-icon.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/react.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/redux.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/javascript.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/html5.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/css.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/tailwindcss-mark.svg' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/nodejs.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/ruby.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/rails.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/heroku.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/git.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/mongodb.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/postgres.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/sass.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/bootstrap.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-        <div>
-        <Image src='/assets/icons/aws.png' 
-    width={65}
-    height={65}
-     priority alt="" />
-        </div>
-    
-     </div>
-  )
-}
+    <div className="grid grid-cols-4 w-full justify-items-center gap-8 mt-10">
+      <div>
+        <Image
+          src="/assets/icons/nextjs-icon.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/javascript.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/python.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/fastAPI.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/react.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/redux.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/html5.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/css.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/tailwindcss-mark.svg"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/nodejs.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/ruby.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/rails.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/heroku.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/git.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/mongodb.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/postgres.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/sass.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/bootstrap.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/aws.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+      <div>
+        <Image
+          src="/assets/icons/figma.png"
+          width={65}
+          height={65}
+          priority
+          alt=""
+        />
+      </div>
+    </div>
+  );
+};
 
-export default Technologies
+export default Technologies;
