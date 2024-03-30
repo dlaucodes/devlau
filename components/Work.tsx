6b5cf5c3873a26534eb379c17ecbfb450dfde714
Chun Lau <dlaucodes@gmail.com>
2024-03-30 15:38:53 -0400

    implement swiper'

--- components/Work.tsx
+++ components/Work.tsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import Link from 'next/link';
+import { Button } from './ui/button';
+
+import {Swiper,} from 'swiper';
+
+import 'swiper/css';
+import 'swiper/css/pagination'
 
 import {Pagination}  from 'swiper/modules';
 import ProjectCard from '@/components/ProjectCard';
@@ -40,17 +47,24 @@ const projectData = [
 
 const Work = () => {
   return (
-    <section>
+    <section className='relative mb-12 xl:mb-48'>
         <div className='container mx-auto'>
-            <div>
-                <h2>
+            <div className='max-w-[400px] mx-auto xl:mx-0 text-center xl:text-left mb-12 xl:h-[400px] flex flex-col justify-center items-center xl:items-start'>
+                <h2 className='section-title mb-4'>
                     Latest Projects
                 </h2>
-                <p>
+                <p className='subtitle mb-8'>
                     Lorem Ipsum
                 </p>
+                <Link href='/projects'>
+                <Button>
+                    All projects
+                </Button>
+                </Link>
             </div>
+            <div>
 
+            </div>
         </div>
     </section>
   )
