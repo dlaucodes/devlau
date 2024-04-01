e1e7049042875c95d48f960c1b819adafe7d7638
Chun Lau <dlaucodes@gmail.com>
2024-03-31 23:38:11 -0400

    typing

--- app/template.tsx
+++ app/template.tsx
@@ -1,30 +1,59 @@
-'use client'
+// 'use client'
+// import useScrollProgress from "@/hooks/useScrollProgress";
+// import {motion} from 'framer-motion';
+
+// const variants = {
+//     hidden: { opacity: 0},
+//     enter: {opacity: 1},
+// }
+
+// const Template = ({children }) => {
+//   const completion = useScrollProgress();
+//   return (
+   
+//         <motion.main 
+//         variants={variants}
+//         initial='hidden'
+//         animate='enter'
+//         transition={{type: 'linear', delay: 0.2, duration: 0.4 }}
+//         >
+//             {children}
+//         </motion.main>
+    
+   
+
+//     ) 
+    
+// }
+
+// export default Template;
+
+import React from 'react'; // Import React from 'react'
+import { motion } from 'framer-motion';
 import useScrollProgress from "@/hooks/useScrollProgress";
-import {motion} from 'framer-motion';
 
 const variants = {
-    hidden: { opacity: 0},
-    enter: {opacity: 1},
+  hidden: { opacity: 0 },
+  enter: { opacity: 1 },
 }
 
-const Template = ({children, ...props}) => {
-  const completion = useScrollProgress();
-  return (
-   
-        <motion.main
-        {...props} 
-        variants={variants}
-        initial='hidden'
-        animate='enter'
-        transition={{type: 'linear', delay: 0.2, duration: 0.4 }}
-        >
-            {children}
-        </motion.main>
-    
-   
+interface TemplateProps {
+  children: React.ReactNode; // Define the type of children prop explicitly
+}
 
-    ) 
-    
+const Template: React.FC<TemplateProps> = ({ children }) => { // Use React.FC to define the component type
+  const completion = useScrollProgress();
+  
+  return (
+    <motion.main
+      variants={variants}
+      initial='hidden'
+      animate='enter'
+      transition={{ type: 'linear', delay: 0.2, duration: 0.4 }}
+    >
+      {children}
+    </motion.main>
+  );
 }
 
 export default Template;
