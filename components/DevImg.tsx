35c78b2fac566c0448c761eb28e082cce47360f3
Chun Lau <dlaucodes@gmail.com>
2024-03-31 23:49:36 -0400

    change DevImg

--- components/DevImg.tsx
+++ components/DevImg.tsx
@@ -1,6 +1,13 @@
 import Image from "next/image";
+import React from "react";
 
-const DevImg = ({ containerStyles, imgSrc }) => {
+// added typing
+interface DevImgProps {
+  containerStyles: string;
+  imgSrc: string;
+}
+
+const DevImg: React.FC<DevImgProps> = ({ containerStyles, imgSrc }) => {
   return (
     <div className={`${containerStyles}`}>
       <Image src={imgSrc} width={400} height={400} priority alt="" />
